fe60d6d7040ee6c9aecd02a1db941d2f2f929c3c
Joss Manger <jossferatu@gmail.com>
2021-10-02 15:05:28 -0400

    Initial Commit

---
+++ Boro WatchKit Extension/BoroApp.swift
@@ -0,0 +1,19 @@
+//
+//  BoroApp.swift
+//  Boro WatchKit Extension
+//
+//  Created by Joss Manger on 10/2/21.
+//
+
+import SwiftUI
+
+@main
+struct BoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
