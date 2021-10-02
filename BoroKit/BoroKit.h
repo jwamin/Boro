76ce728c8269b7a80011117ae43f79a74497ac90
Joss Manger <jossferatu@gmail.com>
2021-10-02 16:09:18 -0400

    basic enum behavior, tests

---
+++ BoroKit/BoroKit.h
@@ -0,0 +1,18 @@
+//
+//  BoroKit.h
+//  BoroKit
+//
+//  Created by Joss Manger on 10/2/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BoroKit.
+FOUNDATION_EXPORT double BoroKitVersionNumber;
+
+//! Project version string for BoroKit.
+FOUNDATION_EXPORT const unsigned char BoroKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoroKit/PublicHeader.h>
+
+
---
+++ BoroKit/BoroKit.swift
@@ -0,0 +1,42 @@
+//
+//  BoroKit.swift
+//  BoroKit
+//
+//  Created by Joss Manger on 10/2/21.
+//
+
+import Foundation
+import CoreLocation
+
+public enum Boro: String {
+  case brooklyn = "Brooklyn"
+  case manhattan = "Manhattan"
+  case queens = "Queens"
+  case bronx = "The Bronx"
+  case statenIsland = "Staten Island"
+  case six
+  case out
+  
+  public init?(placemark: CLPlacemark){
+    
+    guard let adminArea = placemark.administrativeArea, let subLocality = placemark.subLocality else {
+      self.init(rawValue: "out")
+      return
+    }
+    
+    switch adminArea {
+    case "NY":
+      if let cityBoro = Boro(rawValue: subLocality) {
+        self = cityBoro
+        return
+      } else {
+        fallthrough
+      }
+    case "NJ":
+      self = .six
+    default:
+      self = .out
+    }
+  }
+  
+}
