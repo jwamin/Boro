46bb2d65388399230fd2f5cd89b4130d2fb4b592
Joss Manger <jossferatu@gmail.com>
2023-07-23 15:41:57 -0400

    Companion WatchOS target

---
+++ Boro WK Companion Watch App/Boro_WK_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boro_WK_CompanionApp.swift
+//  Boro WK Companion Watch App
+//
+//  Created by Joss Manger on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Boro_WK_Companion_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
